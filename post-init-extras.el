f0b5101439af6e51f7a6d8a54ee4a4f00fa1402f
Sangho Na <sangho@nsh.nz>
2026-02-01 17:17:24 +1300

    Add expand-region dep

--- post-init-extras.el
+++ post-init-extras.el
@@ -1,4 +1,9 @@
 ;; External dependencies
 
+(use-package expand-region
+  :ensure t
+  :bind ("C-x j" . er/expand-region)
+  :custom ((expand-region-contract-fast-key "k")))
+
 (use-package magit
   :ensure t)
