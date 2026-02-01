44e108108a114a04d9115eacfd6360b8ad4df95d
Sangho Na <sangho@nsh.nz>
2026-02-01 18:43:19 +1300

    Add consult, embark, marginalia, orderless and vertico

--- post-init.el
+++ post-init.el
@@ -19,6 +19,22 @@
   :config
   (electric-pair-mode +1))
 
+;; Emacs minibuffer configurations.
+(use-package emacs
+  :custom
+  ;; Enable context menu. `vertico-multiform-mode' adds a menu in the minibuffer
+  ;; to switch display modes.
+  (context-menu-mode t)
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt)))
+
 (use-package frame
   :ensure nil
   :custom
@@ -26,10 +42,16 @@
   :config
   (blink-cursor-mode +1))
 
+;; Prefer vertico
 (use-package icomplete
   :ensure nil
   :config
-  (fido-vertical-mode +1))
+  (fido-vertical-mode -1))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
 
 (use-package repeat
   :ensure nil
